5a92a0a38f4cddfbca9c809feeceb5504d3dff4e
Justin Gaffney <justingaffney@hotmail.com>
2025-03-14 17:13:39 +1000

    Add stream equivalent of examples

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,40 @@ wit_bindgen::generate!({
     world: "example-world"
 });
 
-use wit_bindgen::rt::async_support::FutureReader;
+use wit_bindgen::rt::async_support::{
+    FutureReader,
+    StreamReader
+};
 use crate::exports::example::pkg::example::Guest;
 
 struct Example;
 
 impl Guest for Example {
-    fn test1() -> FutureReader<Result<Vec<String>, ()>> {
+    fn future1() -> FutureReader<Result<Vec<String>, ()>> {
         unimplemented!()
     }
 
-    fn test2() -> FutureReader<Result<Vec::<Vec::<u8>>, ()>> {
+    fn future2() -> FutureReader<Result<Vec::<Vec::<u8>>, ()>> {
         unimplemented!()
     }
 
-    fn test3() -> FutureReader<Vec::<Vec::<Vec::<u8>>>> {
+    fn future3() -> FutureReader<Vec::<Vec::<Vec::<u8>>>> {
+        unimplemented!()
+    }
+    
+    fn stream1() -> StreamReader<Result<Vec<String>, ()>> {
+        unimplemented!()
+    }
+
+    fn stream2() -> StreamReader<Result<Vec::<Vec::<u8>>, ()>> {
+        unimplemented!()
+    }
+
+    fn stream3() -> StreamReader<Vec::<Vec::<Vec::<u8>>>> {
         unimplemented!()
     }
 }
 
 export!(Example);
+
+mod example_world;
